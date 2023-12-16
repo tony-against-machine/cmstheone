d008420a65d9cc1c94f2a7687176eef3c030b269
Tony Skill <tony.against.machine@gmail.com>
2023-12-16 13:43:40 +0300

    The basic route structure has been created

---
+++ app/routes.py
@@ -0,0 +1,75 @@
+from flask import render_template, redirect, request, url_for, flash
+from werkzeug.security import generate_password_hash
+from app import app, db, bcrypt
+from app.models import Article, Users, User, RegistrationForm
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return render_template('create-article.html')
+        except:
+            print('Ошибка добавления статьи')
+    else:
+        return render_template('create-article.html')
+
+
+@app.route('/display-articles')
+def display_articles():
+    articles = Article.query.order_by(Article.created_at.desc()).all()
+    return render_template('display-articles.html', articles=articles)
+
+
+@app.route('/article-more/<int:article_id>/')
+def article_more(article_id):
+    article = Article.query.get_or_404(article_id)
+    return render_template('article-more.html', article=article)
+
+
+@app.route('/user-registration', methods=['POST', 'GET'])
+def user_registration():
+    if request.method == 'POST':
+
+        try:
+            passwd_hash = generate_password_hash(request.form['passwd'])
+            user_email_passwd = Users(user_email=request.form['email'], user_passwd=passwd_hash)
+            db.session.add(user_email_passwd)
+            db.session.commit()
+            print('Пользователь добавлен')
+        except:
+            db.session.rollback()
+            print('Ошибка добавления пользователя в БД')
+
+    return render_template('user-registration.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Твоя учетная запись создана!', 'success')
+        return redirect(url_for('/index'))
+    return render_template('registration.html', form=form)
+
