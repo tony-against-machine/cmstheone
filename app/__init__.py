20076e9f240889dbcc116ce566379ca0672d9e31
Tony Skill <tony.against.machine@gmail.com>
2023-12-16 13:35:22 +0300

    the application and database are initialized

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+secret_key = os.getenv('SECRET_KEY')
+database_uri = os.getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'cmstheonedb.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+from app import models, routes
+
+
+with app.app_context():
+    db.create_all()
