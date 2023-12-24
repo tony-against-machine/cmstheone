924bbb31d88ae7ec4acbb83396b62ed57e93cec9
Tony Skill <tony.against.machine@gmail.com>
2023-12-24 21:51:30 +0300

    the Client model has been deleted

--- app/models.py
+++ app/models.py
@@ -64,8 +64,3 @@ class NoteForm(FlaskForm):
     note = StringField('Note', render_kw={"placeholder": "Enter your note"})
     submit = SubmitField('Add Note')
 
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
