b66ab834234a22d9ddb086d994e5b31cc417741a
Tony Skill <tony.against.machine@gmail.com>
2023-12-25 14:21:50 +0300

    the Client model has been added

--- app/models.py
+++ app/models.py
@@ -36,6 +36,12 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
