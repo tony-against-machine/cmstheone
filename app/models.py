fbc00ec5a662f27aa7633cdac3455cec6126ae6d
Tony Skill <tony.against.machine@gmail.com>
2023-12-26 14:09:13 +0300

    for the phone variable of the Client model, the data type has been changed from Integer to String

--- app/models.py
+++ app/models.py
@@ -39,7 +39,7 @@ class LoginForm(FlaskForm):
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    phone = db.Column(db.Integer(20), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
